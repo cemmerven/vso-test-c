3f6673e0f44ec8a80c5c1c4a864dea5e4bd99241
cemmerven <cemmerven@gmail.com>
2020-04-11 01:07:39 +0000

    test console output

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 
+#include <stdio.h>
+#include <math.h>
 
 int main(int argc, char** argv) {
+    float value = 8;
+    float root = sqrtf( value );
+    printf( "square root of %f is %f\r\n", value, root );
     return 0; 
 }
